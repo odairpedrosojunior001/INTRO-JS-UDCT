8cfc53ccac589638e7b544acb1c9bf8964252b0a
Odair Pedroso <odair.junior@luizalabs.com>
2019-05-07 13:52:34 -0300

    atual

--- dados_variaveis.js
+++ dados_variaveis.js
@@ -5,4 +5,12 @@ var num2 = 7
 console.log("Tenho " + (num1 + num2) + " anos de idade!!!");
 
 
+// index
+var nome = "James";
+console.log(nome[0]);
+console.log(nome[4]);
+console.log(nome[3]);
+console.log(nome[2-1]);
+
 // 
+
