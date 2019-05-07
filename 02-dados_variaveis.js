77e8d43831cc48ff7f8e3c7365a7b00049964041
Odair Pedroso <odair.junior@luizalabs.com>
2019-05-07 13:57:05 -0300

    manipulação

---
+++ 02-dados_variaveis.js
@@ -0,0 +1,16 @@
+
+// manipulação variaveis e strings
+var num1 = 30
+var num2 = 7
+console.log("Tenho " + (num1 + num2) + " anos de idade!!!");
+
+
+// index
+var nome = "James";
+console.log(nome[0]);
+console.log(nome[4]);
+console.log(nome[3]);
+console.log(nome[2-1]);
+
+// 
+
