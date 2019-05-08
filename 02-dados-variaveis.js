d79f6d2a1899d04d187b03157641d15f4502bb09
Odair Pedroso <odair.junior@luizalabs.com>
2019-05-08 09:03:15 -0300

    js

--- 02-dados-variaveis.js
+++ 02-dados-variaveis.js
@@ -57,6 +57,22 @@ console.log(joke);
 var haiku = "Blowing from the west\n\nFallen leaves gather\n\nIn the east.";
 console.log(haiku);
 
+for (var i=1;i<=100;i++){
+    console.log(i);
+  }
+
+
+/* Instruções Quiz:
+Crie uma variável chamada bill e atribua a ela o resultado de 10.25 + 3.99 + 7.15 (não faça a conta você mesmo, deixe o JavaScript fazer isso!). Em seguida, crie uma variável chamada tip e atribua a ela o resultado da multiplicação de bill por uma taxa tip de 15%. Finalmente, some bill e tip e armazene o resultado em uma variável de nome total.
+
+Exiba o valor de total no console do JavaScript.
+
+Dica: 15% na forma decimal é escrito como 0.15_._
+
+DICA: para exibir o valor de total com o símbolo de dólar ($), utilize concatenação de strings. Para arredondar para duas casas decimais, utilize o método toFixed(). Para utilizar toFixed(), passe o número de casas decimais que você deseja ter. Por exemplo, se total for igual a 3.9860, então total.toFixed(2) retornará 3.99.*/
+
+var 
+
 
 
 
